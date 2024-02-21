a03f6e641180c51c7471495fbccde908af672949
baiyuemei <superstart01@163.com>
2024-02-21 15:36:44 +0800

    初始化

---
+++ src/settings.js
@@ -0,0 +1,16 @@
+module.exports = {
+
+  title: '小白白NEW人力资源系统',
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether fix the header
+   */
+  fixedHeader: true,
+
+  /**
+   * @type {boolean} true | false
+   * @description Whether show the logo in sidebar
+   */
+  sidebarLogo: true // 是否显示侧边栏的log
+}
